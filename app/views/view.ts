70365d59a1cc52dcd05f09b0bd37d388a413135f
Natanias souza <nataniasdopb@gmail.com>
2023-02-27 15:48:45 -0300

    fist commit projct

---
+++ app/views/view.ts
@@ -0,0 +1,28 @@
+export abstract class View<T> {
+  protected element: HTMLElement;
+  private escapar = false;
+
+  constructor(seletor: string, escapar?: boolean) {
+    // a "?" faz o parametro ser opcional
+    const element = document.querySelector(seletor)
+    if(element){
+      this.element = element as HTMLElement
+    }else{
+      throw Error(`Seletor ${seletor} não existe no DOM`)
+    }
+    
+    if (escapar) {
+      this.escapar = escapar;
+    }
+  }
+
+  protected abstract template(model: T): string;
+
+  public update(model: T): void {
+    let template = this.template(model);
+    if (this.escapar) {
+      template = template.replace(/<script>[\s\S]*?<\/script>/, "");
+    }
+    this.element.innerHTML = template;
+  }
+}
